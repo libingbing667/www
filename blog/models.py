d58a6edd42a0fe82dff55f8ffc61324553c75297
array <libingbing667@hotmail.com>
2016-12-16 20:45:45 +0800

    个人博客blog

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 # -*- coding=utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 # 用户模型.
