68ab5cdb4e0c09d1a75a148e3acf387abdd7c2e2
array <libingbing667@hotmail.com>
2016-12-17 14:02:39 +0800

    个人博客blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,26 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 from django.contrib import admin
 from models import *
 
 
 class ArticleAdmin(admin.ModelAdmin):
-
-    #fields = ('title','desc','content')
-    list_display = ('title','desc','click_count')
+    # fields = ('title','desc','content')
+    list_display = ('title', 'desc', 'click_count')
     list_display_links = ('title', 'desc', 'click_count')
 
-
+    class Media:
+        js = (
+            '/static/js/kindeditor-4.1.10/kindeditor-min.js',
+            '/static/js/kindeditor-4.1.10/lang/zh_cn.js',
+            '/static/js/kindeditor-4.1.10/config.js',
+        )
 
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Tag)
-admin.site.register(Article,ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Catagory)
 admin.site.register(Comment)
 admin.site.register(Link)
---
+++ static/js/kindeditor-4.1.10/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/12/17.
+ */
+KindEditor.ready(function(K) {
+                 K.create('textarea[name=content]',{
+                    width:800,
+                    height:200,
+
+               });
+
+        });
